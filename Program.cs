eea3ac9d758750acc937e6ad1535c31cdd6cf455
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2023-04-20 14:14:20 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6: \r\n");
+
+            int idade = 23;
+            int idadeAna = idade;
+
+            idade = 20;
+
+            Console.WriteLine("A idade é " + idade + " anos.");
+            Console.WriteLine("A idade é " + idadeAna + " anos.");
+            // Aqui definimos que idadeAna é igual a idade, mas em seguida, alteramos o valor de idade, esta variável passou a receber o valor 20. Quando executamos o código, vemos que idadeAna continua igual a 23, porém, idade terá um novo valor, ou seja o 20.
+
+            Console.ReadLine();
+        }
+    }
+}
